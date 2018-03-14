795eed0ffa23f7ba72a34d086233fb8a1dcbc536
ScorpionVEVO <37373659+ScorpionVEVO@users.noreply.github.com>
2018-03-14 15:44:07 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function(){
+    bot.user.setGame("Je suis bo");
+    console.log("Le bot a bien été connecté");
+})
+
+bot.login("NDIzNDc1ODYzODI1NDE2MjEz.DYq4jg.SwsI6LO1RhryZ7IgQMyyiIg6SS4");
+
+bot.on('message', function (message){
+    if(message.content === '!ping') {
+        message.reply('pong')
+    }
+})
+
+bot.on('message', function (message){
+    if(message.content === '!help')
+    message.channel.sendMessage('Aide du bot discord: ')
+    message.channel.sendMessage('```!help pour affiché l'aide du bot```')
+})
